3ee0441a71da792be3012b8f5fe1852a4461f7ef
Glitch (sticky-stream) <none>
2018-04-04 15:12:20 +0000

    🙍🖇 Checkpoint ./script.js:678137/573

--- script.js
+++ script.js
@@ -32,16 +32,25 @@ var options = [
 
 var mant = "";
 
+var index = function (min, max) {
+  return Math.random(min, max);
+}
 
+var strForAdd = function () {
+      if (mant.length > 0) {
+        let arr = mant.split("");
+        for (i=0; i <= arr.length; i++) {
+          let dubleInd = options.indexOf(arr[i])
+        }
+  }
+  else {
+    return index(0, options.length)
+  }
 button.addEventListener("click", function(){
   if (input.value.length < 1) {
     alert("Пожалуйста напишите как вы будите использовать созданную специально для Вас мантру !");
   } else {
     eight.innerText = "";
-    answer.innerText = function () {
-      if (mant.length > 0) {
-        index = Math.
-        let mantarr = mant.split("")
-  }
+    answer.innerText = strForAdd
 });
 
