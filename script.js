3a11c8f0b172a2b7a1e1d4293309e8ec76dc7f09
Glitch (sticky-stream) <none>
2018-04-04 15:32:29 +0000

    🚘🎉 Checkpoint ./script.js:678137/490

--- script.js
+++ script.js
@@ -37,23 +37,32 @@ var index = function (min, max) {
 }
 
 var strForAdd = function () {
+      // если мантра уже не пустая
       if (mant.length > 0) {
+        // преобразуем её из строки в массив строк
         let arr = mant.split("");
         // индексируем буквы в мантре
         for (i=0; i <= arr.length; i++) {
-          // получаем индексы букв в оптионс уже имеющихся в мантре
+          // ищем дубликаты и получаем индексы этих букв в оптионс
           let dubleInd = options.indexOf(arr[i]);
+          // удаляем дубликаты из оптионс по индексам
           options.splice(dubleInd, dubleInd);
+          // возвращаем рандомную букву из очищеного оптионс
+          return options[Math.random(0, options.length)]
         }
   }
+  // если мантра пустая
   else {
+    // возвращаем рандомную букву из стартового оптионс
     return index(0, options.length)
   }
+  
+
 button.addEventListener("click", function(){
   if (input.value.length < 1) {
     alert("Пожалуйста напишите как вы будите использовать созданную специально для Вас мантру !");
   } else {
     eight.innerText = "";
-    answer.innerText = strForAdd
+    answer.innerText = strForAdd();
 });
 
