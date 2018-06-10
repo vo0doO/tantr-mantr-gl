8cfc63284235445db31b4e7f0b072e637915b619
Glitch (transvist-mantr) <none>
2018-06-10 18:00:20 +0000

    🚛🐟 Checkpoint ./server.js:678137/220

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const app = express();
 
+
+// Ссылаемся на статические файлы
 app.use(express.static('./'));
 
-// Возвращаем 
+// Возвращаем index.html
 app.get("/", function(req, res) {
   res.sendFile("index.html");
 })
 
+// Возвращаем Readme.md
 app.get("/readme", function(req, res) {
   res.sendFile("/app/README.md")
 })
 
+// Возвращаем edit.html
 app.get("/edit", function(req, res) {
   res.sendFile("/app/edit.html")
 })
+
+// Возвращаем concept.html
 app.get("/concept", function(req, res) {
   res.sendFile("/app/concept.html")
 })
 
+// Разворачиваем приложение(определяем порт)
 const listener = app.listen(process.env.PORT, function () {
   console.log("ТВОЕ ПРИЛОЖЕНИЕ РАЗВЕРНУТО НА ПОРТУ " + process.env.PORT)
 })
